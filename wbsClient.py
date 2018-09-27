198b57b453e64d98392337262a9984f269c5753d
halowens <halowens@pal-nat186-40-217.itap.purdue.edu>
2018-09-27 19:05:57 -0400

    Initial commit of websocket server

---
+++ wbsClient.py
@@ -0,0 +1,27 @@
+import asyncio
+import websockets
+
+
+async def receiveData():
+    async with websockets.connect('ws://localhost:5000') as websocket:
+        data = await websocket.recv()
+        print(data)
+while True:
+    asyncio.get_event_loop().run_until_complete(receiveData())
+
+"""
+import asyncio
+import websockets
+
+async def hello():
+    async with websockets.connect(
+            'ws://localhost:8765') as websocket:
+        name = input("What's your name? ")
+
+        await websocket.send(name)
+        print(f"> {name}")
+
+        greeting = await websocket.recv()
+        print(f"< {greeting}")
+
+asyncio.get_event_loop().run_until_complete(hello())"""
---
+++ wbsServer.py
@@ -0,0 +1,25 @@
+import asyncio
+import websockets
+import random
+
+ip = "localhost"
+port = 5000
+data = []
+async def getData():
+    """CODE THAT GETS CAN DATA """
+
+async def sendData(websocketIp):
+    await websocketIp.send(str(data))
+
+async def main(ip, port):
+    """getData and sendData are ran concurrently to allow simultaneously updating and data transmission"""
+    sendData_Task = asyncio.create_task(sendData(ip))
+    getData_Task = asyncio.create_task(getData())
+    await sendData_Task
+    await getData_Task
+
+
+server = websockets.serve(main, ip, port)
+print("Server established at ws://{}:{}".format(ip, port))
+asyncio.get_event_loop().run_until_complete(server)
+asyncio.get_event_loop().run_forever()
