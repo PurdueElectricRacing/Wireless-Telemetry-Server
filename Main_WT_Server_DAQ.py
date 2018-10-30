3477156ecf529e69e0b8b2df1980391e31c7204a
AdamBusch <adamjbusch561@gmail.com>
2018-10-29 23:33:24 -0400

    Add inital data logging
    Output data logs to the ./logs directory and mimic the styel of past
    logs made with the wifican.js file to allow for legacy Matlab
    processing.

--- Main_WT_Server_DAQ.py
+++ Main_WT_Server_DAQ.py
@@ -3,7 +3,9 @@ import asyncio
 import random
 import serial_asyncio
 import time
-import datetime
+from datetime import datetime
+import logging
+import os.path
 
 # This will run on startup on the RPI.
 
@@ -13,6 +15,47 @@ import datetime
 
 ip = '192.168.4.1'
 port = 5000
+
+start_date_time = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+
+start_path = os.path.abspath(os.path.dirname(__file__))
+log_path = os.path.join(start_path, 'logs',
+                        start_date_time + '.txt')
+# configure logger to not format messages
+logging.basicConfig(level=logging.DEBUG, filename=log_path,
+                    filemode="a+", format='%(message)s')
+
+data_logger = logging.getLogger(__name__)
+logging.getLogger("asyncio").setLevel(logging.WARNING)
+
+data_logger.info(start_date_time)
+
+# Max resolution (ms) for data frames to be logged
+max_time_diff_ms = 100
+start_time = int(round(time.time() * 1000))
+last_time = start_time
+multi_frame_message = ''
+
+
+# Logs a set of CAN frames in a time interval to a log file
+async def log_CAN_data(timestamp, can_id, length, message):
+    current_time = int(round(time.time() * 1000))
+
+    # Wait for serial connection to stabalize, avoid garbage data
+    if current_time - start_time < 1500:
+        return
+
+    global last_time, multi_frame_message
+
+    delta_time = current_time - last_time
+    single_frame = ',' + can_id + ',' + length + ',' + message
+
+    if(delta_time >= max_time_diff_ms):
+        data_logger.info(multi_frame_message)
+        last_time = current_time
+        multi_frame_message = str(current_time - start_time) + single_frame
+    else:
+        multi_frame_message += single_frame
 
 
 # Used to setup and open CANdapter, only runs once.
@@ -40,24 +83,26 @@ async def rec_serial_data(serial_connection):
         m_id = message[0:3]
         m_len = message[3:4]
         m_message = message[4:-4]
-        m_time_stamp = str(datetime.datetime.now())
+        m_timestamp = str(datetime.now())
+
+        await log_CAN_data(m_timestamp, m_id, m_len, m_message)
 
         data = {
             "id": m_id,
             "length": m_len,
             "message": m_message,
-            "timestamp": m_time_stamp
+            "timestamp": m_timestamp
             }
 
         await server.send_data(data)
 
 
 async def main(loop):
-    reader, writer = await serial_asyncio.open_serial_connection(
+    serial_reader, serial_writer = await serial_asyncio.open_serial_connection(
         url='/dev/ttyUSB0', baudrate=2500000)
 
-    read_task = asyncio.ensure_future(rec_serial_data(reader))
-    write_task = asyncio.ensure_future(write_serial_data(writer))
+    read_task = asyncio.ensure_future(rec_serial_data(serial_reader))
+    write_task = asyncio.ensure_future(write_serial_data(serial_writer))
 
     server_task = asyncio.ensure_future(server.get_server(ip, port))
 
