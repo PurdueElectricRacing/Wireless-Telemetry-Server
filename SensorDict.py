af48098b796a14fcf6d173a6a02c25bf808eaa5f
AdamBusch <adamjbusch561@gmail.com>
2019-01-26 02:26:05 -0500

    Add potential IDs and byte indicies.
    First revision of CAN information for new sensors

--- SensorDict.py
+++ SensorDict.py
@@ -4,11 +4,12 @@ import csv
 # about each individual sensor on the car as well as its can ID
 # and data offset used to parse the specific sensor's data
 
-sensor_file = csv.DictReader(open("SensorDict.csv"))
+sensor_file = csv.DictReader(open('SensorDict.csv'))
 
-# Sensor ID values are in base 16
-sensors = {(int(row['id'], 16), row['name']) for row in sensor_file}
-
-# Start by printing all data imported by the file
-for s in sensors:
-    print(s)
+for row in sensor_file:
+    if row:
+        row['id'] = int(row['id'].strip(), 16)
+        row['start_byte'] = int(row['start_byte'].strip())
+        row['end_byte'] = int(row['end_byte'].strip())
+        row['name'] = row['name'].strip()
+        print(row['comment'])
