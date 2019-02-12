6b1c7a4f63d097526eebe6c2b64d4cd694ffdf53
AdamBusch <adamjbusch561@gmail.com>
2019-02-11 22:19:21 -0500

    Add documentation to generated header file
    This will make time-stamping in main module easier to manage if each
    header file is dated at the top.

--- can_mgmt/FormatSensorInfo.py
+++ can_mgmt/FormatSensorInfo.py
@@ -3,12 +3,13 @@ from SensorDict import CANParser
 
 parser = CANParser()
 
-
-# When run as main, output current sensor CSV data to a format
-# for pasting into the Wiki
+# Display formatted text for either pasting into google sheets
+# or on the wiki for reference.
 if __name__ == '__main__':
     if len(sys.argv) >= 2:
 
+        # Output for google sheets with the following output:
+        # Name, ID, , SensorName (ByteN), SensorName (ByteN-1), ... SensorName (Byte0)
         if sys.argv[1] == 'sheets':
             outputList = {}
 
@@ -37,25 +38,26 @@ if __name__ == '__main__':
             for canID in sorted(outputList):
                 print(outputList[canID])
 
+        # Outputs all data in the wiki markdown format for a table
+        # ID, Name, [MSB:LSB]
         elif sys.argv[1] == 'wiki':
             print("{| class=\"wikitable\"")
             print("|-")
             print("!ID")
             print("!Name")
             print("![MSB:LSB]")
-            print("!Comments")
             print("|-")
-            for row in sensor_file:
-                if row:
+            for canIDDec in parser.sensorLib:
+                idEntry = parser.sensorLib[canIDDec]
+                canID = hex(canIDDec)
 
-                    row['id'] = (row['id'].strip())
-                    row['LSB'] = int(row['LSB'].strip())
-                    row['MSB'] = int(row['MSB'].strip())
-                    row['name'] = row['name'].strip()
+                for sensorName in idEntry:
+                    sensorData = idEntry[sensorName]
+                    msb = int(sensorData[0])
+                    lsb = int(sensorData[1])
 
-                    print("|", row['id'])
-                    print("|", row['name'])
-                    print("|[" + str(row['MSB']) + ":" + str(row['LSB'])+"]")
-                    print("|", row['comments'])
+                    print("|", canID)
+                    print("|", sensorName)
+                    print("|[" + str(msb) + ":" + str(lsb)+"]")
                     print("|-")
             print("|}")
--- can_mgmt/GenerateHeaders.py
+++ can_mgmt/GenerateHeaders.py
@@ -1,10 +1,22 @@
 import pandas as pd
+import datetime as dt
 
 # Name of the column with "C-friendly" names
 name_column = "name"
 
 sensors_df = pd.read_csv("SensorDict.csv", sep=",", header=0, dtype="str")
 savefile = open("CANID.h", "w+")
+
+header = "/* Generated on " + str(dt.datetime.now()) + "\n"
+header += "* This file contains all of the constants that are\n"
+header += "* being used on the CAN bus.\n"
+header += "*\n"
+header += "* Each message has an ID with specified sensors that\n"
+header += "* lie on each message from bytes *_START_BYTE to\n"
+header += "* *_END_BYTE.\n*/\n\n"
+header += "#ifndef CANID_H\n#define CANID_H"
+
+savefile.write(header)
 
 lastID = ""
 for row in range(0, len(sensors_df)):
@@ -21,4 +33,5 @@ for row in range(0, len(sensors_df)):
     savefile.write("#define " + sensor_name_and_type + "_END_BYTE " +
                    sensors_df.loc[row, "LSB"] + "\n")
     lastID = ID
+savefile.write("#endif /* CANID_H */")
 savefile.close()
