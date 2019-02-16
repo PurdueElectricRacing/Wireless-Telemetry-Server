d186847fe228a8a27335e15258a5430efa47e531
jacklecroy <3073035+jacklecroy@users.noreply.github.com>
2019-02-16 12:15:51 -0500

    Combines header gen. and sensordict updater
    One script will now update
    SensorDict.csv and CANID.h. It will
    ignore rows that don't have correctly
    formatted names.

--- can_mgmt/update-SensorDict.py
+++ can_mgmt/update-SensorDict.py
@@ -1,39 +1,39 @@
 import requests
 import pandas as pd
-import math
-# Maybe you can do this with the csv library
-# But Pandas is way easier to use
+import datetime as dt
 
-file_id = "195Y2cf9C2mrA6QCLuLJv3NHsCqTLi-3Wd07q4T5rXuM"
-output_file = "SavedGoogleDrive.csv"
-# Index of the header row:
-ignore_rows = 5
+SPREADSHEET_FILE_ID = "195Y2cf9C2mrA6QCLuLJv3NHsCqTLi-3Wd07q4T5rXuM"
+OUTPUT_CSV_FILE = "SensorDict.csv"
+OUTPUT_HEADER_FILE = "CANID.h"
+# Rows before the header are skipped.
+# The row after the header will also be skipped because it is an example row.
+HEADER_ROW = 5
+BYTES_LIST = ["BYTE0", "BYTE1", "BYTE2", "BYTE3",
+              "BYTE4", "BYTE5", "BYTE6", "BYTE7"]
 
 # This only gets the FIRST SHEET in the document
 # If there is more than one sheet, make sure the first sheet is correct
 response = requests.get("https://docs.google.com/spreadsheet/ccc?key=" +
-                        file_id + "&output=csv")
+                        SPREADSHEET_FILE_ID + "&output=csv")
 # Check that status code = 200
 response.raise_for_status()
 
 # The CSV spreadsheet is requests.content
 # Turn it into file:
-savefile = open(output_file, "wb")
-savefile.write(response.content)
-savefile.close()
+savefile_csv = open(OUTPUT_CSV_FILE, "wb")
+savefile_csv.write(response.content)
+savefile_csv.close()
 
 # Read file as dataframe.
-content_df = pd.read_csv(output_file, sep=",", header=ignore_rows, dtype="str")
+content_df = pd.read_csv(OUTPUT_CSV_FILE, sep=",", header=HEADER_ROW,
+                         dtype="str")
 
 # Drop unwanted columns:
 content_df = content_df.drop(["TRANSMITTING MODULE", "DESCRIPTION / SENSOR",
                               "AddMethd"], axis="columns")
 
-bytes_list = ["BYTE0", "BYTE1", "BYTE2", "BYTE3",
-              "BYTE4", "BYTE5", "BYTE6", "BYTE7"]
-
 # Drop example row:
-content_df = content_df.drop(0)
+# content_df = content_df.drop(0)
 content_df = content_df.set_index("ID", drop=True)
 
 # New dataframe for output
@@ -41,13 +41,11 @@ output_df = pd.DataFrame(None, columns=["id", "MSB", "LSB", "name"])
 
 for index, row in content_df.iterrows():
     last_cell = None
-    if row.empty:
-        print("Unable to parse " + str(row))
-        continue
-    for column in bytes_list:
+    for column in BYTES_LIST:
         item = str(row[column])
-        if(item == "nan"):
-            continue
+        # Mediocre check that the names are formatted correctly:
+        if(item.find("_") == -1):
+            break
         paren_start = item.find("(")
         # If there is an opening parenthesis, remove all text after it
         if(paren_start != -1):
@@ -65,4 +63,36 @@ for index, row in content_df.iterrows():
         last_cell = item
 
 # Save new output file.
-output_df.to_csv(output_file, index=None, header=True)
+output_df.to_csv(OUTPUT_CSV_FILE, index=None, header=True)
+savefile_h = open(OUTPUT_HEADER_FILE, "w+")
+
+header = """/* Generated on {}
+* This file contains all of the constants that are
+* being used on the CAN bus.
+*
+* Each message has an ID with specified sensors that
+* lie on each message from bytes *_START_BYTE to
+* *_END_BYTE.
+*/
+
+#ifndef CANID_H
+#define CANID_H""".format(str(dt.datetime.now()))
+
+savefile_h.write(header)
+
+last_ID = ""
+for index, row in output_df.iterrows():
+    sensor_name_and_type = row["name"]
+    last_underscore = sensor_name_and_type.rindex("_")
+    sensor_name = sensor_name_and_type[0:last_underscore]
+    ID = row["id"]
+    if last_ID != ID:
+        savefile_h.write("\n#define {}_CAN_ID {}\n".format(sensor_name, ID))
+    savefile_h.write("#define {}_START_BYTE {}\n".format(sensor_name_and_type,
+                                                         row["MSB"]))
+    savefile_h.write("#define {}_END_BYTE {}\n".format(sensor_name_and_type,
+                                                       row["LSB"]))
+    last_ID = ID
+
+savefile_h.write("#endif /* CANID_H */")
+savefile_h.close()
