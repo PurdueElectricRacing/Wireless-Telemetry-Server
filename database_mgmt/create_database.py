7524f0f824ab41d6c6c801aa4281d9aadb4aa209
Ryan Schwartz <schwar95@purdue.edu>
2019-09-07 12:36:40 -0400

    Fix database queries to create the database
    Relates to #16

--- database_mgmt/create_database.py
+++ database_mgmt/create_database.py
@@ -1,28 +1,23 @@
 import os
 import pandas as pd
-from dotenv import load_dotenv
-load_dotenv()
+from database import Database
 
-sensor_dict = pd.read_csv("../can_mgmt/SensorDict.csv", header=0,  dtype="str")
+sensor_dict = pd.read_csv('can_mgmt/SensorDict.csv', header=0,  dtype='str')
 
 db = Database()
 cursor = db.get_cursor()
-cursor.execute("CREATE DATABASE sensors")
-cursor.execute("USE sensors")
+cursor.execute('CREATE DATABASE sensors')
+cursor.execute('USE sensors')
 
-current_id = "#####"
+current_id = '#####'
 for index, row in sensor_dict.iterrows():
-    if row["id"][3] == current_id[3]:
-        add_query = "ADD COLUMN {} INT(16) NULL".format(row["name"])
+    if row['id'][3] == current_id[3]:
+        add_query = 'ALTER TABLE {} ADD COLUMN {} INT(16) NULL'.format(current_id[1:4], row['name'])
         cursor.execute(add_query)
     else:
-        current_id = row["id"]
-        table_name = current_id[0:3]
-        create_query = "CREATE TABLE {}".format(table_name)
-        alter_query = "ALTER TABLE {}".format(table_name)
-        timestamp_query = "ADD COLUMN timestamp DATETIME NOT NULL"
-        add_query = "ADD COLUMN {} INT(16) NULL".format(row["name"])
+        current_id = row['id']
+        table_name = current_id[1:4] # Tables can't start with a 0x (no starting integer), so start with x instead
+        create_query = 'CREATE TABLE {} (timestamp DATETIME(3) NOT NULL)'.format(table_name)
+        alter_query = 'ALTER TABLE {} ADD COLUMN {} INT(16) NULL'.format(table_name, row['name'])
         cursor.execute(create_query)
         cursor.execute(alter_query)
-        cursor.execute(timestamp_query)
-        cursor.execute(add_query)
