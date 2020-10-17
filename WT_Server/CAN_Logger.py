4a0cdb9ee636ef5d78b988438119fb2788374aa8
Luke Oxley <lukeoxley9@gmail.com>
2020-10-17 11:44:44 -0400

    Changed CAN_Logger to log files in csv format

--- WT_Server/CAN_Logger.py
+++ WT_Server/CAN_Logger.py
@@ -2,6 +2,7 @@ import os.path
 from datetime import datetime
 import time
 import sys
+import csv
 
 log_path = ''
 
@@ -9,13 +10,22 @@ log_path = ''
 max_time_diff_ms = 10
 start_time = int(round(time.time() * 1000))
 last_time = start_time
-multi_frame_message = ''
+multi_frame_message = []
 
 def log_string(message):
     global log_path
     with open(log_path, 'a+') as logfile:
         logfile.write(message)
 
+#Same concept as SensorDict's save_data_to_csv(    
+def log_csv(input_list):
+    global log_path
+    with open(log_path, 'a+') as fid:
+            logfile = csv.DictWriter(fid, 
+                                    fieldnames=["timestamp", "id", "data"],
+                                    extrasaction="ignore")
+            for dictionary in input_list:
+                logfile.writerow(dictionary)
 
 def create_logfile():
     print("[INFO] Creating logfile...")
@@ -25,10 +35,12 @@ def create_logfile():
     start_path = os.path.abspath(os.path.dirname(__file__))
 
     log_path = os.path.join(start_path, 'logs',
-                        start_date_time + '.txt')
+                        start_date_time + '.csv')
 
-    log_string(start_date_time)
-    print("[INFO] Logfile created: {}.txt".format(start_date_time))
+    #log_string(start_date_time)
+    log_csv([{"id": "id", "data": "data", "timestamp": "timestamp"}])
+
+    print("[INFO] Logfile created: {}.csv".format(start_date_time))
     
 
 # Logs a set of CAN frames in a time interval to a log file
@@ -45,12 +57,13 @@ def log_CAN_data(can_id, message):
     global last_time, multi_frame_message, log_path
 
     delta_time = current_time - last_time
-    single_frame = ';' + str(can_id) + ',' + str(message)
+    single_frame = {"id": str(can_id), "data": str(message), "timestamp": str(current_time-start_time)}
+
+    multi_frame_message.append(single_frame)
 
     if(delta_time >= max_time_diff_ms):
-        log_string("\n" + multi_frame_message)
+        log_csv(multi_frame_message)
         last_time = current_time
+        print(str(multi_frame_message))
+        multi_frame_message.clear()
 
-        multi_frame_message = str(current_time - start_time) + single_frame
-    else:
-        multi_frame_message += single_frame
